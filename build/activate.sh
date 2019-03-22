888065396965f9b9d8230857c1edccb2448f05bd
qiushaoxi <qiushaoxi@163.com>
2019-03-22 12:08:38 +0800

    add activate.sh

---
+++ build/activate.sh
@@ -0,0 +1,8 @@
+enucli -u http://3.1.39.190:10802 --wallet-url=http://127.0.0.1:8901 push action ubicandidate activate '["loverockinme"]' -p ubicandidate
+enucli -u http://3.1.39.190:10802 --wallet-url=http://127.0.0.1:8901 push action ubicandidate activate '["allrightenus"]' -p ubicandidate
+enucli -u http://3.1.39.190:10802 --wallet-url=http://127.0.0.1:8901 push action ubicandidate activate '["enumaster123"]' -p ubicandidate
+enucli -u http://3.1.39.190:10802 --wallet-url=http://127.0.0.1:8901 push action ubicandidate activate '["zsylxqlovenu"]' -p ubicandidate
+enucli -u http://3.1.39.190:10802 --wallet-url=http://127.0.0.1:8901 push action ubicandidate activate '["masterenuubi"]' -p ubicandidate
+enucli -u http://3.1.39.190:10802 --wallet-url=http://127.0.0.1:8901 push action ubicandidate activate '["543215432154"]' -p ubicandidate
+enucli -u http://3.1.39.190:10802 --wallet-url=http://127.0.0.1:8901 push action ubicandidate activate '["enuqbdingo55"]' -p ubicandidate
+enucli -u http://3.1.39.190:10802 --wallet-url=http://127.0.0.1:8901 push action ubicandidate activate '["lixiaonuo123"]' -p ubicandidate
